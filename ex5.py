e104b4b191234a3940a3a72872f1391a13029829
higoramaral <amarahigor@gmail.com>
2026-05-28 20:32:25 -0300

    refatora código para coletar e verificar a presença do número 30 em uma lista de entradas

--- ex5.py
+++ ex5.py
@@ -1,2 +1,19 @@
-lista = []
+i = 0
+soma = []
 
+while i < 100:
+    numero = int(input("Digite um número: "))
+    soma.append(numero)
+    i += 1
+
+i = 0
+encontrou = False
+
+while i < 100:
+    if soma[i] == 30:
+        print("Tem 30 na posição:", i)
+        encontrou = True
+    i += 1
+
+if not encontrou:
+    print("Não tem 30 na lista")
